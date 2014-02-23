3f3faebbf87d2d67e6e3c0137653578619dfc8d6
Martin Panter <vadmium  gmailcom>
2014-02-23 06:06:32 +0000

    Simplify with OffsetRenderer._map()

--- vector/base.py
+++ vector/base.py
@@ -9,32 +9,21 @@ class Renderer:
 
 class OffsetRenderer:
     def __init__(self, renderer, offset):
-        self.renderer = renderer
-        self.offset = offset
+        self._renderer = renderer
+        self._offset = offset
     
-    def line(self, *points):
-        if self.offset:
-            points = (map(operator.add, point, self.offset) for
-                point in points)
-        self.renderer.line(*points)
+    def line(self, a=None, b=None, *pos, **kw):
+        self._renderer.line(self._map(a), self._map(b), *pos, **kw)
+    def circle(self, r, centre=None, *pos, **kw):
+        self._renderer.circle(r, self._map(centre), *pos, **kw)
+    def polygon(self, points, *pos, **kw):
+        self._renderer.polygon(map(self._map, points), *pos, **kw)
+    def rectangle(self, dim, start=None, *pos, **kw):
+        self._renderer.rectangle(dim, self._map(start), *pos, **kw)
     
-    def circle(self, r, centre=None):
-        if centre or self.offset:
-            centre = centre or (0, 0)
-            centre = map(operator.add, centre, self.offset or (0, 0))
-            self.renderer.circle(r, centre)
-        else:
-            self.renderer.circle(r)
-    
-    def polygon(self, points):
-        if self.offset:
-            points = (map(operator.add, point, self.offset) for
-                point in points)
-        self.renderer.polygon(points)
-    
-    def rectangle(self, dim, start=None):
-        if start or self.offset:
-            start = map(operator.add, start or (0, 0), self.offset or (0, 0))
-            self.renderer.rectangle(dim, start)
-        else:
-            self.renderer.rectangle(dim)
+    def _map(self, point):
+        if not self._offset:
+            return point
+        if not point:
+            return self._offset
+        return map(operator.add, point, self._offset)
