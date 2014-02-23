cf044f66b9b7c0c4e8ba72a2008aefa10d3bb852
Martin Panter <vadmium  gmailcom>
2014-02-23 02:24:42 +0000

    Turn “vector” into package with separate rendering modules

--- vector/svg.py
+++ vector/svg.py
@@ -1,49 +1,8 @@
 from xml.sax.saxutils import XMLGenerator
 from contextlib import contextmanager
-from tkinter import Tk
-import tkinter
-import operator
+from . import base
 
-class Renderer:
-    def set_objects(self, objects):
-        pass
-    
-    def draw(self, object, offset=None):
-        object.draw(OffsetRenderer(self, offset))
-
-class OffsetRenderer:
-    def __init__(self, renderer, offset):
-        self.renderer = renderer
-        self.offset = offset
-    
-    def line(self, *points):
-        if self.offset:
-            points = (map(operator.add, point, self.offset) for
-                point in points)
-        self.renderer.line(*points)
-    
-    def circle(self, r, centre=None):
-        if centre or self.offset:
-            centre = centre or (0, 0)
-            centre = map(operator.add, centre, self.offset or (0, 0))
-            self.renderer.circle(r, centre)
-        else:
-            self.renderer.circle(r)
-    
-    def polygon(self, points):
-        if self.offset:
-            points = (map(operator.add, point, self.offset) for
-                point in points)
-        self.renderer.polygon(points)
-    
-    def rectangle(self, dim, start=None):
-        if start or self.offset:
-            start = map(operator.add, start or (0, 0), self.offset or (0, 0))
-            self.renderer.rectangle(dim, start)
-        else:
-            self.renderer.rectangle(dim)
-
-class SvgRenderer(Renderer):
+class Renderer(base.Renderer):
     def __init__(self, size, line=0.4, colour="black"):
         self.svg = XMLGenerator(encoding="UTF-8", short_empty_elements=True)
         self.svg.startElement("svg", {
@@ -108,41 +67,6 @@ class SvgRenderer(Renderer):
     
     def finish(self):
         self.svg.endElement("svg")
-
-class TkRenderer(Renderer):
-    def __init__(self, size, line=0.4, colour="black"):
-        self.colour = colour
-        
-        root = Tk()
-        self.scaling = root.call("tk", "scaling") * 72 / 25.4  # pixels/mm
-        self.linewidth = line * self.scaling
-        self.canvas = tkinter.Canvas(root,
-            relief=tkinter.SUNKEN, borderwidth=1,
-            background="white",
-            height=size[1] * self.scaling, width=size[0] * self.scaling,
-        )
-        self.canvas.pack(fill=tkinter.BOTH, expand=True)
-    
-    def line(self, a=(0, 0), b=(0, 0)):
-        points = (x * self.scaling for point in (a, b) for x in point)
-        self.canvas.create_line(*points, fill=self.colour, width=self.linewidth)
-    
-    def circle(self, r, centre=(0, 0)):
-        coords = tuple((o - r, o + r) for o in centre)
-        points = (x[i] * self.scaling for i in range(2) for x in coords)
-        self.canvas.create_oval(*points, fill=self.colour, width=0)
-    
-    def polygon(self, points):
-        points = tuple(x * self.scaling for point in points for x in point)
-        self.canvas.create_polygon(points)
-    
-    def rectangle(self, dim, start=(0, 0)):
-        coords = tuple((x, o + x) for (x, o) in zip(start, dim))
-        points = (x[i] * self.scaling for i in range(2) for x in coords)
-        self.canvas.create_rectangle(*points, fill=self.colour, width=0)
-    
-    def finish(self):
-        tkinter.mainloop()
 
 @contextmanager
 def element(xml, name, *pos, **kw):
---
+++ vector/tk.py
@@ -0,0 +1,38 @@
+from tkinter import Tk
+import tkinter
+from . import base
+
+class Renderer(base.Renderer):
+    def __init__(self, size, line=0.4, colour="black"):
+        self.colour = colour
+        
+        root = Tk()
+        self.scaling = root.call("tk", "scaling") * 72 / 25.4  # pixels/mm
+        self.linewidth = line * self.scaling
+        self.canvas = tkinter.Canvas(root,
+            relief=tkinter.SUNKEN, borderwidth=1,
+            background="white",
+            height=size[1] * self.scaling, width=size[0] * self.scaling,
+        )
+        self.canvas.pack(fill=tkinter.BOTH, expand=True)
+    
+    def line(self, a=(0, 0), b=(0, 0)):
+        points = (x * self.scaling for point in (a, b) for x in point)
+        self.canvas.create_line(*points, fill=self.colour, width=self.linewidth)
+    
+    def circle(self, r, centre=(0, 0)):
+        coords = tuple((o - r, o + r) for o in centre)
+        points = (x[i] * self.scaling for i in range(2) for x in coords)
+        self.canvas.create_oval(*points, fill=self.colour, width=0)
+    
+    def polygon(self, points):
+        points = tuple(x * self.scaling for point in points for x in point)
+        self.canvas.create_polygon(points)
+    
+    def rectangle(self, dim, start=(0, 0)):
+        coords = tuple((x, o + x) for (x, o) in zip(start, dim))
+        points = (x[i] * self.scaling for i in range(2) for x in coords)
+        self.canvas.create_rectangle(*points, fill=self.colour, width=0)
+    
+    def finish(self):
+        tkinter.mainloop()
