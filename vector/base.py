c2d8e45945e35d441637b05f4ce4518c48f80b0c
Martin Panter <vadmium  gmailcom>
2014-03-12 04:35:56 +0000

    Default roundrect() implementation based on circles and rectangles

--- vector/base.py
+++ vector/base.py
@@ -15,6 +15,53 @@ class Renderer:
             kw.update(b=(0, b))
         self.line((0, a), **kw)
     
+    def roundrect(self, r, a, b=None, *, offset=None,
+    fill=None, outline=None, **kw):
+        if not all(r):
+            return self.rectangle(a, b,
+                offset=offset, fill=fill, outline=outline, **kw)
+        
+        if b:
+            (ax, ay) = a
+            (bx, by) = b
+        else:
+            (ax, ay) = (0, 0)
+            (bx, by) = a
+        
+        # Only positive dimensions are considered
+        (rx, ry) = r
+        cax = ax + rx
+        cay = ay + ry
+        cbx = bx - rx
+        cby = by - ry
+        
+        with self.offset(offset):
+            # TODO: circles not good enough if fill=None; need arcs
+            c0 = (cax, cay)
+            self.circle(r, c0, fill=fill, outline=outline, **kw)
+            c = (cbx, cay)
+            if c != c0:
+                self.circle(r, c, fill=fill, outline=outline, **kw)
+            c = (cax, cby)
+            if c != c0:
+                self.circle(r, c, fill=fill, outline=outline, **kw)
+            c = (cbx, cby)
+            if c != c0:
+                self.circle(r, c, fill=fill, outline=outline, **kw)
+            
+            if cby != cay:
+                if fill:
+                    self.rectangle((ax, cay), (bx, cby), fill=fill)
+                if outline or not fill:
+                    self.vline(cay, cby, offset=(ax, 0), colour=outline)
+                    self.vline(cay, cby, offset=(bx, 0), colour=outline)
+            if cbx != cax:
+                if fill:
+                    self.rectangle((cax, ay), (cbx, by), fill=fill)
+                if outline or not fill:
+                    self.hline(cax, cbx, offset=(0, ay), colour=outline)
+                    self.hline(cax, cbx, offset=(0, by), colour=outline)
+    
     def addobjects(self, objects):
         pass
     
