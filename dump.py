7ca6b98fc28bdef179814ed3c289a998d2ebba82
Martin Panter <vadmium  gmailcom>
2015-07-08 08:05:09 +0000

    Open file from command line argument

--- dump.py
+++ dump.py
@@ -1,15 +1,16 @@
 #! /usr/bin/env python3
 
 import altium
-from sys import stdin
+from sys import argv
 
-def main():
-    objects = altium.read(stdin.buffer)
+def main(file):
+    with open(file, "rb") as file:
+        objects = altium.read(file)
     for o in objects:
         print(repr(o))
 
 if __name__ == "__main__":
     try:
-        main()
+        main(*argv[1:])
     except (KeyboardInterrupt, ConnectionError):
         raise SystemExit(1)
