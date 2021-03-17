5f49b06df0a1514fd4577805a0c1b0541bc48637
Martin Panter <vadmium  gmailcom>
2021-03-17 02:08:19 +0000

    Fix TK arc drawing with width specified

--- vector/base.py
+++ vector/base.py
@@ -170,8 +170,8 @@ class Subview(View):
         return self._parent.cubicbezier(*map(self._rotate, points),
             colour=colour or self._colour,
         **kw)
-    def arc(self, r, start, end, offset=None, *, colour=None):
-        kw = dict(offset=offset)
+    def arc(self, r, start, end, offset=None, *, colour=None, **kw):
+        kw.update(offset=offset)
         r = self._rotate(r)
         self._map_offset(kw)
         start += self._rotation * 90
